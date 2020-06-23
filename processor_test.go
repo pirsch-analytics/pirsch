7dd52f32d7acd4df535dc7a9ad96da4c7ee877d4
Marvin Blum <marvin.blum@emvi.com>
2020-06-23 20:30:27 +0200

    Finished tests for basic statistics.

--- processor_test.go
+++ processor_test.go
@@ -39,6 +39,10 @@ func checkVisitorCount(t *testing.T, store *PostgresStore) {
 	if len(visitors) != 2 {
 		t.Fatalf("Two visitors per day must have been created, but was: %v", len(visitors))
 	}
+
+	if visitors[0].Visitors != 3 || visitors[1].Visitors != 3 {
+		t.Fatal("Visitors not as expected")
+	}
 }
 
 func checkVisitorCountHour(t *testing.T, store *PostgresStore) {
@@ -51,29 +55,71 @@ func checkVisitorCountHour(t *testing.T, store *PostgresStore) {
 	if len(visitors) != 4 {
 		t.Fatalf("Four visitors per hour must have been created, but was: %v", len(visitors))
 	}
+
+	if visitors[0].DayAndHour.Hour() != 7 ||
+		visitors[1].DayAndHour.Hour() != 8 ||
+		visitors[2].DayAndHour.Hour() != 9 ||
+		visitors[3].DayAndHour.Hour() != 10 {
+		t.Fatal("Times not as expected")
+	}
+
+	if visitors[0].Visitors != 2 ||
+		visitors[1].Visitors != 1 ||
+		visitors[2].Visitors != 2 ||
+		visitors[3].Visitors != 1 {
+		t.Fatal("Visitors not as expected")
+	}
 }
 
 func checkLanguageCount(t *testing.T, store *PostgresStore) {
 	var visitors []VisitorsPerLanguage
 
-	if err := store.DB.Select(&visitors, `SELECT * FROM "visitors_per_language" ORDER BY day`); err != nil {
+	if err := store.DB.Select(&visitors, `SELECT * FROM "visitors_per_language" ORDER BY day, language`); err != nil {
 		t.Fatal(err)
 	}
 
 	if len(visitors) != 4 {
 		t.Fatalf("Four visitors per language must have been created, but was: %v", len(visitors))
 	}
+
+	if visitors[0].Language != "de" ||
+		visitors[1].Language != "en" ||
+		visitors[2].Language != "en" ||
+		visitors[3].Language != "jp" {
+		t.Fatal("Languages not as expected")
+	}
+
+	if visitors[0].Visitors != 1 ||
+		visitors[1].Visitors != 2 ||
+		visitors[2].Visitors != 2 ||
+		visitors[3].Visitors != 1 {
+		t.Fatal("Visitors not as expected")
+	}
 }
 
 func checkPageViewCount(t *testing.T, store *PostgresStore) {
 	var visitors []VisitorsPerPage
 
-	if err := store.DB.Select(&visitors, `SELECT * FROM "visitors_per_page" ORDER BY day`); err != nil {
+	if err := store.DB.Select(&visitors, `SELECT * FROM "visitors_per_page" ORDER BY day, path`); err != nil {
 		t.Fatal(err)
 	}
 
 	if len(visitors) != 4 {
 		t.Fatalf("Four visitors per page must have been created, but was: %v", len(visitors))
+	}
+
+	if visitors[0].Path != "/" ||
+		visitors[1].Path != "/page" ||
+		visitors[2].Path != "/" ||
+		visitors[3].Path != "/different-page" {
+		t.Fatal("Paths not as expected")
+	}
+
+	if visitors[0].Visitors != 2 ||
+		visitors[1].Visitors != 1 ||
+		visitors[2].Visitors != 2 ||
+		visitors[3].Visitors != 1 {
+		t.Fatal("Visitors not as expected")
 	}
 }
 
@@ -84,7 +130,7 @@ func createTestdata(t *testing.T, store Store) {
 	createHit(t, store, "fp3", "/page", "de", "ua3", day(2020, 6, 22, 8))
 	createHit(t, store, "fp4", "/", "en", "ua4", day(2020, 6, 23, 9))
 	createHit(t, store, "fp5", "/", "en", "ua5", day(2020, 6, 23, 9))
-	createHit(t, store, "fp6", "/page", "de", "ua6", day(2020, 6, 23, 10))
+	createHit(t, store, "fp6", "/different-page", "jp", "ua6", day(2020, 6, 23, 10))
 }
 
 func createHit(t *testing.T, store Store, fingerprint, path, lang, userAgent string, time time.Time) {
@@ -102,5 +148,5 @@ func createHit(t *testing.T, store Store, fingerprint, path, lang, userAgent str
 }
 
 func day(year, month, day, hour int) time.Time {
-	return time.Date(year, time.Month(month), day, hour, 0, 0, 0, time.Local)
+	return time.Date(year, time.Month(month), day, hour, 0, 0, 0, time.UTC)
 }
