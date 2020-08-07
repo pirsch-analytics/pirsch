23d1a2fb6a877882fda8860e54c5a7493991af44
Marvin Blum <marvin.blum@emvi.com>
2020-08-07 23:41:29 +0200

    Improved extracting client IP from proxy headers and added X-Real-IP header in addition.

---
+++ ip.go
@@ -0,0 +1,81 @@
+package pirsch
+
+import (
+	"net"
+	"net/http"
+	"strings"
+)
+
+// Headers and corresponding parser to look up the real client IP.
+// They will be check in order, the first non-empty one will be picked,
+// or else the remote address is selected.
+var ipHeaders = []ipHeader{
+	{"X-Forwarded-For", parseXForwardedForHeader},
+	{"Forwarded", parseForwardedHeader},
+	{"X-Real-IP", parseXRealIPHeader},
+}
+
+type ipHeader struct {
+	header string
+	parser func(string) string
+}
+
+// getIP returns the IP from given request.
+// It will try to extract the real client IP from headers if possible.
+func getIP(r *http.Request) string {
+	ip := r.RemoteAddr
+
+	for _, header := range ipHeaders {
+		value := r.Header.Get(header.header)
+
+		if value != "" {
+			parsedIP := header.parser(value)
+
+			if parsedIP != "" {
+				ip = parsedIP
+				break
+			}
+		}
+	}
+
+	if strings.Contains(ip, ":") {
+		host, _, err := net.SplitHostPort(ip)
+
+		if err != nil {
+			return ip
+		}
+
+		return host
+	}
+
+	return ip
+}
+
+func parseForwardedHeader(value string) string {
+	parts := strings.Split(value, ",")
+	parts = strings.Split(parts[0], ";")
+
+	for _, part := range parts {
+		kv := strings.Split(part, "=")
+
+		if len(kv) == 2 {
+			k := strings.ToLower(strings.TrimSpace(kv[0]))
+			v := strings.TrimSpace(kv[1])
+
+			if k == "for" {
+				return v
+			}
+		}
+	}
+
+	return ""
+}
+
+func parseXForwardedForHeader(value string) string {
+	parts := strings.Split(value, ",")
+	return strings.TrimSpace(parts[0])
+}
+
+func parseXRealIPHeader(value string) string {
+	return value
+}
