2e60301ef75e95215d7819566105e661494bf9a5
Marvin Blum <marvin.blum@emvi.com>
2020-08-09 21:46:39 +0200

    ...

--- schema/postgres/test.sql
+++ schema/postgres/test.sql
@@ -1,5 +1,5 @@
 -- This file sets up the test schema.
 
-SET SCHEMA 'pirsch';
+SET search_path TO pirsch;
 \i /go/src/github.com/emvi/pirsch/schema/postgres/schema.sql
 \i /go/src/github.com/emvi/pirsch/schema/postgres/v1.2.0.sql
