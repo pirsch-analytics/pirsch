d32e2a2f911eff7e84128169b4f8ea8313d54816
Marvin Blum <marvin.blum@emvi.com>
2020-08-09 21:43:22 +0200

    Fixed paths.

--- schema/postgres/test.sql
+++ schema/postgres/test.sql
@@ -1,3 +1,5 @@
+-- This file sets up the test schema.
+
 SET SCHEMA 'pirsch';
-\i schema.sql
-\i v1.2.0.sql
+\i /go/src/github.com/emvi/pirsch/schema/postgres/schema.sql
+\i /go/src/github.com/emvi/pirsch/schema/postgres/v1.2.0.sql
