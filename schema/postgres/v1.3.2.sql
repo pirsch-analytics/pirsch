17ed2b963fc291d8fd95a7865f3be5bb28f08f10
Marvin Blum <marvin.blum@emvi.com>
2020-08-14 00:17:49 +0200

    Renamed missing database object referer -> referrer.

--- schema/postgres/v1.3.2.sql
+++ schema/postgres/v1.3.2.sql
@@ -1,1 +1,4 @@
 ALTER TABLE "visitors_per_referer" RENAME TO "visitors_per_referrer";
+ALTER SEQUENCE visitors_per_referer_id_seq RENAME TO "visitors_per_referrer_id_seq";
+ALTER INDEX visitors_per_referer_day_index RENAME TO visitors_per_referrer_day_index;
+ALTER INDEX visitors_per_referer_tenant_id_index RENAME TO visitors_per_referrer_tenant_id_index;
