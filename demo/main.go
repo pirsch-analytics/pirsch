0bcf1b355c01ce0518ffa3089a2780593ea75e21
Marvin Blum <marvin.blum@emvi.com>
2020-08-30 20:25:16 +0200

    Fixed demo code.

--- demo/main.go
+++ demo/main.go
@@ -20,9 +20,9 @@ func main() {
 	}
 
 	// create a new tracker and processor using postgres as its data store
-	store := pirsch.NewPostgresStore(db)
+	store := pirsch.NewPostgresStore(db, nil)
 	tracker := pirsch.NewTracker(store, "salt", nil)
-	processor := pirsch.NewProcessor(store)
+	processor := pirsch.NewProcessor(store, nil)
 	pirsch.RunAtMidnight(func() {
 		if err := processor.Process(); err != nil {
 			panic(err)
