956377621eee310592e516f2cc8ce8dabd2d10cb
Marvin Blum <marvin.blum@emvi.com>
2020-09-19 20:14:14 +0200

    Added GeoDB to map IPs to countries.

--- geodb.go
+++ geodb.go
@@ -3,8 +3,10 @@ package pirsch
 import (
 	"archive/tar"
 	"compress/gzip"
+	"github.com/oschwald/maxminddb-golang"
 	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -15,8 +17,53 @@ const (
 	geoLite2Permalink     = "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=LICENSE_KEY&suffix=tar.gz"
 	geoLite2LicenseKey    = "LICENSE_KEY"
 	geoLite2TarGzFilename = "GeoLite2-Country.tar.gz"
-	geoLite2Filename      = "GeoLite2-Country.mmdb"
+
+	// GeoLite2Filename is the default filename of the GeoLite2 database.
+	GeoLite2Filename = "GeoLite2-Country.mmdb"
 )
+
+// GeoDB maps IPs to their geo location based on MaxMinds GeoLite2 or GeoIP2 database.
+type GeoDB struct {
+	db *maxminddb.Reader
+}
+
+// NewGeoDB creates a new GeoDB for given database file.
+// If you use this in combination with GetGeoLite2, you should pass in the path to GeoLite2Filename (including the filename).
+func NewGeoDB(file string) (*GeoDB, error) {
+	db, err := maxminddb.Open(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	geoDB := &GeoDB{
+		db: db,
+	}
+	return geoDB, nil
+}
+
+// CountryCode looks up the country code for given IP.
+// If the IP is invalid it will return an empty string.
+// The country code is returned in lowercase.
+func (db *GeoDB) CountryCode(ip string) string {
+	parsedIP := net.ParseIP(ip)
+
+	if parsedIP == nil {
+		return ""
+	}
+
+	record := struct {
+		Country struct {
+			ISOCode string `maxminddb:"iso_code"`
+		} `maxminddb:"country"`
+	}{}
+
+	if err := db.db.Lookup(parsedIP, &record); err != nil {
+		return ""
+	}
+
+	return strings.ToLower(record.Country.ISOCode)
+}
 
 // GetGeoLite2 downloads and unpacks the MaxMind GeoLite2 database.
 // The tarball is downloaded and unpacked at the provided path. The directories will created if required.
@@ -89,8 +136,8 @@ func unpackGeoLite2(path string) error {
 			return err
 		}
 
-		if filepath.Base(header.Name) == geoLite2Filename {
-			out, err := os.Create(filepath.Join(path, geoLite2Filename))
+		if filepath.Base(header.Name) == GeoLite2Filename {
+			out, err := os.Create(filepath.Join(path, GeoLite2Filename))
 
 			if err != nil {
 				return err
--- geodb_test.go
+++ geodb_test.go
@@ -6,25 +6,35 @@ import (
 	"testing"
 )
 
-const (
-	// The license key needs to be set for the tests!
-	testLicensekey = ""
-)
-
 func TestGetGeoLite2(t *testing.T) {
-	if testLicensekey == "" {
+	// this test is disabled if the license key is empty
+	licenseKey := os.Getenv("GEOLITE2_LICENSE_KEY")
+
+	if licenseKey == "" {
 		return
 	}
 
-	if err := GetGeoLite2("geodb", testLicensekey); err != nil {
+	if err := GetGeoLite2("geodb", licenseKey); err != nil {
 		t.Fatalf("GeoLite2 DB must have been downloaded and unpacked, but was: %v", err)
 	}
 
-	if _, err := os.Stat(filepath.Join("geodb", geoLite2Filename)); err != nil {
+	if _, err := os.Stat(filepath.Join("geodb", GeoLite2Filename)); err != nil {
 		t.Fatal("GeoLite2 database must exist")
 	}
 
 	if _, err := os.Stat(filepath.Join("geodb", geoLite2TarGzFilename)); !os.IsNotExist(err) {
 		t.Fatalf("GeoLite2 database tarball must not exist anymore, but was: %v", err)
+	}
+}
+
+func TestGeoDB_CountryCode(t *testing.T) {
+	db, err := NewGeoDB(filepath.Join("geodb/GeoIP2-Country-Test.mmdb"))
+
+	if err != nil {
+		t.Fatalf("Geo DB must have been loaded, but was: %v", err)
+	}
+
+	if out := db.CountryCode("81.2.69.142"); out != "gb" {
+		t.Fatalf("Country code for GB must have been returned, but was: %v", out)
 	}
 }
