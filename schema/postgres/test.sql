29e98a3f9e254cd85a0ca5087a92bc47a7fde018
Marvin Blum <6115423+Kugelschieber@users.noreply.github.com>
2020-10-16 13:45:22 +0200

    Fixed test schema setup paths.

--- schema/postgres/test.sql
+++ schema/postgres/test.sql
@@ -1,11 +1,11 @@
 -- This file sets up the test schema.
 
 SET search_path TO public;
-\i /go/src/github.com/emvi/pirsch/schema/postgres/v1.0.0.sql
-\i /go/src/github.com/emvi/pirsch/schema/postgres/v1.2.0.sql
-\i /go/src/github.com/emvi/pirsch/schema/postgres/v1.3.0.sql
-\i /go/src/github.com/emvi/pirsch/schema/postgres/v1.3.2.sql
-\i /go/src/github.com/emvi/pirsch/schema/postgres/v1.4.0.sql
-\i /go/src/github.com/emvi/pirsch/schema/postgres/v1.4.3.sql
-\i /go/src/github.com/emvi/pirsch/schema/postgres/v1.5.0.sql
-\i /go/src/github.com/emvi/pirsch/schema/postgres/v1.6.0.sql
+\i /go/src/github.com/pirsch-analytics/pirsch/schema/postgres/v1.0.0.sql
+\i /go/src/github.com/pirsch-analytics/pirsch/schema/postgres/v1.2.0.sql
+\i /go/src/github.com/pirsch-analytics/pirsch/schema/postgres/v1.3.0.sql
+\i /go/src/github.com/pirsch-analytics/pirsch/schema/postgres/v1.3.2.sql
+\i /go/src/github.com/pirsch-analytics/pirsch/schema/postgres/v1.4.0.sql
+\i /go/src/github.com/pirsch-analytics/pirsch/schema/postgres/v1.4.3.sql
+\i /go/src/github.com/pirsch-analytics/pirsch/schema/postgres/v1.5.0.sql
+\i /go/src/github.com/pirsch-analytics/pirsch/schema/postgres/v1.6.0.sql
