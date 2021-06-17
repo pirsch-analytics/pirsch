182488f0f93ed9f6178bdc208d2c1741ea2b6e91
Marvin Blum <marvin.blum@emvi.com>
2021-06-17 22:19:33 +0200

    Fixed schema migration.

--- schema/0002_nullable_fields.up.sql
+++ schema/0002_nullable_fields.up.sql
@@ -1,3 +1,13 @@
+ALTER TABLE hit UPDATE "session" = 0 where "session" is null;
+ALTER TABLE hit UPDATE referrer = '' where referrer is null;
+ALTER TABLE hit UPDATE referrer_name = '' where referrer_name is null;
+ALTER TABLE hit UPDATE referrer_icon = '' where referrer_icon is null;
+ALTER TABLE hit UPDATE utm_source = '' where utm_source is null;
+ALTER TABLE hit UPDATE utm_medium = '' where utm_medium is null;
+ALTER TABLE hit UPDATE utm_campaign = '' where utm_campaign is null;
+ALTER TABLE hit UPDATE utm_content = '' where utm_content is null;
+ALTER TABLE hit UPDATE utm_term = '' where utm_term is null;
+
 ALTER TABLE "hit" MODIFY COLUMN "session" DateTime('UTC') DEFAULT 0;
 ALTER TABLE "hit" MODIFY COLUMN "referrer" String DEFAULT '';
 ALTER TABLE "hit" MODIFY COLUMN "referrer_name" String DEFAULT '';
