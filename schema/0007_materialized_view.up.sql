bd9686b39446686d0b77ccf1f632962cc2385a8e
Marvin Blum <marvin.blum@emvi.com>
2021-10-18 12:29:40 +0200

    Removed event_duration_seconds from events.

--- schema/0007_materialized_view.up.sql
+++ schema/0007_materialized_view.up.sql
@@ -1,6 +1,7 @@
 ALTER TABLE event DROP COLUMN entry_path;
 ALTER TABLE event DROP COLUMN page_views;
 ALTER TABLE event DROP COLUMN is_bounce;
+ALTER TABLE event DROP COLUMN event_duration_seconds;
 
 CREATE MATERIALIZED VIEW sessions
 ENGINE = AggregatingMergeTree()
