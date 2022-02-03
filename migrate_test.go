239002d2ad8816b47237ddb1cca85f710d129295
Marvin Blum <marvin.blum@emvi.com>
2022-02-03 16:53:59 +0100

    Use correct test database schema.

--- migrate_test.go
+++ migrate_test.go
@@ -6,5 +6,5 @@ import (
 )
 
 func TestMigrate(t *testing.T) {
-	assert.NoError(t, Migrate("clickhouse://127.0.0.1:9000?x-multi-statement=true"))
+	assert.NoError(t, Migrate("clickhouse://127.0.0.1:9000?database=pirschtest&x-multi-statement=true"))
 }
