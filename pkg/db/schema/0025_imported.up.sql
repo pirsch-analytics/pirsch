3489d79fe946dc40d435a48732d9fa0213b0b548
Marvin Blum <marvin.blum@emvi.com>
2024-07-22 22:41:31 +0200

    Added data model and migration for imported statistics.

---
+++ pkg/db/schema/0025_imported.up.sql
@@ -0,0 +1,189 @@
+CREATE TABLE "imported_browser"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "browser" String,
+    "visitors" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE "imported_campaign"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "campaign" String,
+    "visitors" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE "imported_city"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "city" String,
+    "visitors" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE "imported_country"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "country_code" String,
+    "visitors" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE "imported_device"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "category" String,
+    "visitors" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE "imported_entry_page"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "entry_path" String,
+    "visitors" UInt32,
+    "sessions" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE "imported_exit_page"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "exit_path" String,
+    "visitors" UInt32,
+    "sessions" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE "imported_language"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "language" String,
+    "visitors" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE "imported_medium"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "medium" String,
+    "visitors" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE "imported_os"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "os" String,
+    "visitors" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE "imported_page"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "path" String,
+    "visitors" UInt32,
+    "page_views" UInt32,
+    "sessions" UInt32,
+    "bounces" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE "imported_referrer"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "referrer" String,
+    "visitors" UInt32,
+    "sessions" UInt32,
+    "bounces" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE "imported_region"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "region" String,
+    "visitors" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE "imported_source"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "source" String,
+    "visitors" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE "imported_visitors"
+(
+    "client_id" UInt64,
+    "date" Date,
+    "visitors" UInt32,
+    "page_views" UInt32,
+    "sessions" UInt32,
+    "bounces" UInt32,
+    "session_duration" UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(date)
+ORDER BY (client_id, date)
+SETTINGS index_granularity = 8192;
--- pkg/db/testing.go
+++ pkg/db/testing.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
@@ -15,6 +16,7 @@ func Connect() *Client {
 		SSLSkipVerify:      true,
 		Debug:              false,
 		MaxOpenConnections: 1,
+		dev:                true,
 	}
 
 	if err := Migrate(dbConfig); err != nil {
@@ -39,31 +41,73 @@ func Disconnect(client *Client) {
 
 // CleanupDB clears all database tables.
 func CleanupDB(t *testing.T, client *Client) {
-	_, err := client.Exec(`ALTER TABLE "page_view" DELETE WHERE 1=1`)
-	assert.NoError(t, err)
-	_, err = client.Exec(`ALTER TABLE "session" DELETE WHERE 1=1`)
-	assert.NoError(t, err)
-	_, err = client.Exec(`ALTER TABLE "event" DELETE WHERE 1=1`)
-	assert.NoError(t, err)
-	_, err = client.Exec(`ALTER TABLE "request" DELETE WHERE 1=1`)
-	assert.NoError(t, err)
-	time.Sleep(time.Millisecond * 50)
+	if !client.dev {
+		panic("client not in dev mode")
+	}
+
+	tables := []string{
+		"page_view",
+		"session",
+		"event",
+		"request",
+		"imported_browser",
+		"imported_campaign",
+		"imported_city",
+		"imported_country",
+		"imported_device",
+		"imported_entry_page",
+		"imported_exit_page",
+		"imported_language",
+		"imported_medium",
+		"imported_os",
+		"imported_page",
+		"imported_referrer",
+		"imported_region",
+		"imported_source",
+		"imported_visitors",
+	}
+
+	for _, table := range tables {
+		_, err := client.Exec(fmt.Sprintf(`ALTER TABLE "%s" DELETE WHERE 1=1`, table))
+		assert.NoError(t, err)
+	}
+
+	time.Sleep(time.Millisecond * 100)
 }
 
 // DropDB drops all database tables.
 func DropDB(t *testing.T, client *Client) {
-	_, err := client.Exec(`DROP TABLE IF EXISTS "page_view"`)
-	assert.NoError(t, err)
-	_, err = client.Exec(`DROP TABLE IF EXISTS "session"`)
-	assert.NoError(t, err)
-	_, err = client.Exec(`DROP TABLE IF EXISTS "event"`)
-	assert.NoError(t, err)
-	_, err = client.Exec(`DROP TABLE IF EXISTS "event_new"`)
-	assert.NoError(t, err)
-	_, err = client.Exec(`DROP TABLE IF EXISTS "event_backup"`)
-	assert.NoError(t, err)
-	_, err = client.Exec(`DROP TABLE IF EXISTS "request"`)
-	assert.NoError(t, err)
-	_, err = client.Exec(`DROP TABLE IF EXISTS "schema_migrations"`)
-	assert.NoError(t, err)
+	if !client.dev {
+		panic("client not in dev mode")
+	}
+
+	tables := []string{
+		"page_view",
+		"session",
+		"event",
+		"event_new",
+		"event_backup",
+		"request",
+		"schema_migrations",
+		"imported_browser",
+		"imported_campaign",
+		"imported_city",
+		"imported_country",
+		"imported_device",
+		"imported_entry_page",
+		"imported_exit_page",
+		"imported_language",
+		"imported_medium",
+		"imported_os",
+		"imported_page",
+		"imported_referrer",
+		"imported_region",
+		"imported_source",
+		"imported_visitors",
+	}
+
+	for _, table := range tables {
+		_, err := client.Exec(fmt.Sprintf(`DROP TABLE IF EXISTS "%s"`, table))
+		assert.NoError(t, err)
+	}
 }
